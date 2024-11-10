15d81a6b25dab1c2c016c59db1443924afd32aaa
Alex Stamper <asbp4@umsystem.edu>
2024-11-09 18:04:58 -0600

    created template program

--- cli/main.py
+++ cli/main.py
@@ -1,8 +1,19 @@
+#!/usr/bin/env python3
+
+
+import psycopg
+import argparse
 import sys
 
 
+DATABASE_NAME = "multimedia_db"
+DATABASE_USER = "postgres"
+
+
 def main(*args) -> None:
-    pass
+    with psycopg.connect(f"dbname={DATABASE_NAME} user={DATABASE_USER}") as conn:
+        with conn.cursor() as cur:
+            pass
 
 
 if __name__ == "__main__":
