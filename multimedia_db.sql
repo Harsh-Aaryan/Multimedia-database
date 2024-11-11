c808b86cf4710bda0d07a62059641351c256a117
Alex Stamper <asbp4@umsystem.edu>
2024-11-11 12:56:51 -0600

    improved datatypes for increased flexibility

--- multimedia_db.sql
+++ multimedia_db.sql
@@ -18,16 +18,16 @@ CREATE DATABASE multimedia_db
 
 CREATE TABLE user_data (
     id INTEGER PRIMARY KEY,
-    username CHAR(80) NOT NULL,
-    email CHAR(80),
-    password CHAR(80) NOT NULL,
+    username VARCHAR(80) NOT NULL,
+    email VARCHAR(80),
+    password VARCHAR(80) NOT NULL,
     access_level INTEGER
 );
 
 CREATE TABLE media (
     id INTEGER PRIMARY KEY,
-    title CHAR(80) NOT NULL,
-    time_added_posix INTEGER NOT NULL,
+    title VARCHAR(80) NOT NULL,
+    time_added_posix BIGINT NOT NULL,
     release_year INTEGER
 );
 
@@ -35,30 +35,30 @@ CREATE TABLE renting (
     id INTEGER PRIMARY KEY,
     user_id INTEGER REFERENCES user_data(id),
     media_id INTEGER REFERENCES media(id),
-    start_time_posix INTEGER NOT NULL,
-    end_time_posix INTEGER,
+    start_time_posix BIGINT NOT NULL,
+    end_time_posix BIGINT,
     CHECK (end_time_posix > start_time_posix)
 );
 
 CREATE TABLE book (
     media_id INTEGER PRIMARY KEY REFERENCES media(id),
-    author CHAR(80),
-    publisher CHAR(80),
+    author VARCHAR(80),
+    publisher VARCHAR(80),
     isbn INTEGER NOT NULL UNIQUE
 );
 
 CREATE TABLE movie (
     media_id INTEGER PRIMARY KEY REFERENCES media(id),
-    director CHAR(80),
-    publisher CHAR(80),
-    genre CHAR(80),
+    director VARCHAR(80),
+    publisher VARCHAR(80),
+    genre VARCHAR(80),
     duration_seconds INTEGER CHECK (duration_seconds > 0)
 );
 
 CREATE TABLE music (
     media_id INTEGER PRIMARY KEY REFERENCES media(id),
-    artist CHAR(80),
-    album CHAR(80),
-    genre CHAR(80),
+    artist VARCHAR(80),
+    album VARCHAR(80),
+    genre VARCHAR(80),
     duration_seconds INTEGER CHECK (duration_seconds > 0)
 );
