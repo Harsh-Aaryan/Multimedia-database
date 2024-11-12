5ac6df222432f012ff45b6ff74d1cda8889a7ef9
Alex Stamper <asbp4@umsystem.edu>
2024-11-12 12:04:16 -0600

    improved search results

--- cli/remove.py
+++ cli/remove.py
@@ -8,7 +8,7 @@ import sys
 
 
 def main(*args) -> None:
-    deletion_queue = [id[0] for id in search.search(args[1], args[2], args[3], "id")]
+    deletion_queue = [id[0] for id in search.search(args[1], args[2], args[3], True)]
 
     with psycopg.connect(f"dbname={DATABASE_NAME} user={DATABASE_USER}") as conn:
         with conn.cursor() as cur:
--- cli/search.py
+++ cli/search.py
@@ -6,10 +6,31 @@ import psycopg
 import sys
 
 
-def search(table: str, column: str, value: str, output_columns: str="*") -> list[tuple]:
+def search(table: str, column: str, query: str, return_only_id: bool=False) -> list[tuple]:
+    query = query.lower()
+
+    formatted_query = None
+
+    if return_only_id:
+        formatted_query = psycopg.sql.SQL("SELECT id FROM {0} WHERE LOWER ({1}) LIKE {2}")
+        formatted_query = formatted_query.format(
+            psycopg.sql.Identifier(table),
+            psycopg.sql.Identifier(column),
+            f"%%{query}%%"
+        )
+
+    else:
+        formatted_query = psycopg.sql.SQL("SELECT * FROM {0} WHERE LOWER ({1}) LIKE {2}")
+        formatted_query = formatted_query.format(
+            psycopg.sql.Identifier(table),
+            psycopg.sql.Identifier(column),
+            f"%%{query}%%"
+        )
+
+
     with psycopg.connect(f"dbname={DATABASE_NAME} user={DATABASE_USER}") as conn:
         with conn.cursor() as cur:
-            cur.execute(f"SELECT {output_columns} FROM {table} WHERE {column} = %s;", (value,))
+            cur.execute(formatted_query.as_string())
 
             return cur.fetchall()
 
